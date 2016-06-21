a72349911e91c65ee22dcfffb5952a62bea5545c
suttida <suttida5762110020@gmail.com>
2016-06-21 14:31:09 +0700

    syn Json

--- app/src/main/java/com/neu/suttida/bookshop/MainActivity.java
+++ app/src/main/java/com/neu/suttida/bookshop/MainActivity.java
@@ -1,17 +1,105 @@
 package com.neu.suttida.bookshop;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class MainActivity extends AppCompatActivity {
+    //Explicit
+    private EditText userEditTex,passwordEditText;
+    private String userString,passwoetString;
+    private static final String urlJSON = "http://swiftcodingthai.com/neu/get_user.php";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Windget
+        userEditTex = (EditText)findViewById(R.id.editText4);
+        passwordEditText = (EditText)findViewById(R.id.editText5);
+
+
     } //Main Method
+
+    //create Inner vClass
+    private class MySynchronize extends AsyncTask<Void, Void, String> {
+
+        private Context context;
+        private String urLString;
+
+        public MySynchronize(Context context, String urLString) {
+            this.context = context;
+            this.urLString = urLString;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urLString).build();
+                Response response =okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+
+            }catch (Exception e){
+                return null;
+            }
+
+        } //doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d("BookShopV1","JSON ==> "+ s);
+
+        } //onPost
+    } //Class
+
+    public  void clickSignIn(View view){
+        //Get Value form Edit text
+        userString = userEditTex.getText().toString().trim();
+        passwoetString = passwordEditText.getText().toString().trim();
+         //check space
+        if (userString.equals("") || passwoetString.equals("") ) {
+            //Have Space
+            MyAlert myAlert =new MyAlert();
+            myAlert.myDialog(this," Have Space","Please Fill All Blank");
+
+        } else {
+
+            searchUserAnPassword();
+
+            //No Space
+
+
+        }
+
+
+
+
+    } // clickSignIn
+
+    private void searchUserAnPassword() {
+
+        MySynchronize mySynchronize = new MySynchronize(this, urlJSON);
+        mySynchronize.execute();
+
+    } //searchUser
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this,SignUpActivity.class));
